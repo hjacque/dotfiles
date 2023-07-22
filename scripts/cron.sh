fc8515c4dbee7d8f70d39aef1f89c4a10708f536
hippolyte42 <hippolyte.jacque@gmail.com>
2023-07-22 20:23:08 +0200

    cron script - validation & quit on keypress

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-[[ $# -eq 2 ]] || { echo "ERROR: expect arg1: program - arg2: rate"; exit 1; }; while true; do clear; $1; sleep $2; done
+[[ $# -eq 2 ]] || { echo "ERROR: expected - arg1: program, arg2: rate"; exit 1; }
+while true; do clear; $1; read -t $2 -n 1 input; ! [[ $? = 0 ]] || ! [[ $input = "q" ]] || exit 1; done;
