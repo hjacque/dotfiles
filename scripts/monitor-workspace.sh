7a16984ccafbfe207c0179360e498037c3c51920
hippolyte42 <hippolyte.jacque@gmail.com>
2023-07-23 01:16:22 +0200

    monitor-workspace script - network-scan script to layout

--- scripts/monitor-workspace.sh
+++ scripts/monitor-workspace.sh
@@ -4,4 +4,5 @@ re='^[0-9]$'; [[ $# -eq 1 ]] && [[ $1 =~ $re ]] || { echo "ERROR: expected - arg
 i3-msg "workspace $1; append_layout ~/.config/i3/monitor-workspace.json"
 gnome-terminal -- zsh -c "~/scripts/cron.sh neofetch 60; exec zsh;"
 gnome-terminal -- zsh -c "htop; exec zsh"
+gnome-terminal -- zsh -c "~/scripts/cron.sh ~/scripts/network-scan.sh 60; exec zsh;"
 gnome-terminal -- zsh -c "cmatrix; exec zsh"
