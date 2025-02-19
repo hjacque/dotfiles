7ac84444c9843bbc906b36918bb1a9811ccd2d69
Hippolyte Jacque <hippolyte.jacque@gmail.com>
2025-02-19 16:43:16 +0100

    fix(monitor-workspace script): no tildes in quotes

--- scripts/monitor-workspace.sh
+++ scripts/monitor-workspace.sh
@@ -2,7 +2,7 @@
 re='^[0-9]$'; [[ $# -eq 1 ]] && [[ $1 =~ $re ]] || { echo "ERROR: expected - arg1: workspace id"; exit 1; }
 
 i3-msg "workspace $1; append_layout ~/.config/i3/monitor-workspace.json"
-gnome-terminal -- zsh -c "~/scripts/cron.sh neofetch 60; exec zsh;"
+gnome-terminal -- zsh -c "$HOME/scripts/cron.sh neofetch 60; exec zsh;"
 gnome-terminal -- zsh -c "htop; exec zsh"
-gnome-terminal -- zsh -c "~/scripts/cron.sh ~/scripts/network-scan.sh 60; exec zsh;"
+gnome-terminal -- zsh -c "$HOME/scripts/cron.sh ~/scripts/network-scan.sh 60; exec zsh;"
 gnome-terminal --zoom=1.5 -- zsh -c "unimatrix --asynchronous --flashers --character-list=m --color=red; exec zsh"
